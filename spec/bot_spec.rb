a3a310c9c159023c35bc46888a42c3173001cdb3
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2017-01-09 18:32:39 -0300

    Adding Rspec and a stubspec

---
+++ spec/bot_spec.rb
@@ -0,0 +1,9 @@
+require './vacaciones_bot'
+RSpec.describe VacacionesBot do
+  describe "#score" do
+    it "returns 0 for an all gutter game" do
+
+        expect(1 + 1).to eq(2)
+    end
+  end
+end
