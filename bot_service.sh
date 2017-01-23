b61030e549fbfdf77520244d2fa5edb46d1bd8c3
jbci <jbari@ciudadanointeligente.org>
2017-01-23 10:10:11 -0300

    added sh script for launching

---
+++ bot_service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+logfile="bot.log"
+
+echo "vacaciones_bot launch script starting..."
+
+pid=$(ps x | grep ruby| grep vacaciones | awk '{print $1;}')
+echo "pid of process: "
+echo $pid 
+tmp=$(echo $pid | wc -w)
+echo $tmp
+
+
+case "$tmp" in
+
+0)  echo "starting vacaciones_bot"
+    ruby vacaciones.rb > bot_output.log &
+    ;;
+1)  echo "vacaciones_bot running, all OK"
+    ;;
+*)  echo "error"
+    ;;
+esac
