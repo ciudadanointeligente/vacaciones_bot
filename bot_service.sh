8886292f85a1d6ac527a835aa69deb7db78e749d
jdgarrido <damian.garrido.munoz@gmail.com>
2017-01-23 10:46:35 -0300

    update

--- bot_service.sh
+++ bot_service.sh
@@ -6,7 +6,7 @@ echo "vacaciones_bot launch script starting..."
 
 pid=$(ps x | grep ruby| grep vacaciones | awk '{print $1;}')
 echo "pid of process: "
-echo $pid 
+echo $pid
 tmp=$(echo $pid | wc -w)
 echo $tmp
 
@@ -14,7 +14,7 @@ echo $tmp
 case "$tmp" in
 
 0)  echo "starting vacaciones_bot"
-    ruby vacaciones.rb > bot_output.log &
+    ruby ~/vacaciones_bot/vacaciones.rb > ~/vacaciones_bot/bot_output.log &
     ;;
 1)  echo "vacaciones_bot running, all OK"
     ;;
